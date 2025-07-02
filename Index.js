971badaa7eaf5efcd6a8a53b3cfdd9267258cb32
Agyemang8172 <godfredagyemang31052005@gmail.com>
2025-07-02 00:43:21 +0000

    new Changes

--- Index.js
+++ Index.js
@@ -1,14 +1,20 @@
 // Select the elements
-const textInput = document.getElementById( "textInput");
-const  charCount = document.getElementById("charCount")
+const textInput = document.getElementById("textInput")
+const charCount = document.getElementById("charCount")
+const  warning =document.getElementById("warning")
 
+const maxLimit=20;
 
-const updateCharCount = ()   =>  {
-   const length = textInput.value.length;
- 
-   charCount.textContent = length === 0
-   ? "Characters : 0"
-   : `Characters : ${length}`;
-}
+ const updateCharCount =  ()  => {
+    const length = textInput.value.length
+    
+     charCount.textContent = length === 0
+     ? "Characters : 0"
+     : `Characters : ${length}/${maxLimit}`
 
-textInput.addEventListener("input",updateCharCount)
+     length > maxLimit 
+     ? warning.classList.remove("hidden")
+     : warning.classList.add("hidden")
+  }
+
+  textInput.addEventListener("input", updateCharCount);
